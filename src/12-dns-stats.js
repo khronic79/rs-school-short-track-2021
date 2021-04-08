c032736d9833e52c609408c94cd0d610e757b596
khronic79 <rustam.ptashinskiy@gmail.com>
2021-04-08 23:21:17 +0300

    feat: make 12th task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,30 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const d = domains;
+  const dnsStat = {};
+  for (let i = 0; i < d.length; i++) {
+    let pos = 1;
+    let str = '';
+    while (pos > -1) {
+      const len = d[i].length;
+      pos = d[i].lastIndexOf('.');
+      if (pos > -1) {
+        str = `${str}.${d[i].slice(pos + 1, len)}`;
+        if (!dnsStat[str]) {
+          dnsStat[str] = 1;
+        } else { dnsStat[str]++; }
+        d[i] = d[i].slice(0, pos);
+      } else {
+        str = `${str}.${d[i]}`;
+        if (!dnsStat[str]) {
+          dnsStat[str] = 1;
+        } else { dnsStat[str]++; }
+      }
+    }
+  }
+  return dnsStat;
 }
 
 module.exports = getDNSStats;
