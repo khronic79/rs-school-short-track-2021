7d5f58e9a706938372eece06eca11223560e687f
khronic79 <rustam.ptashinskiy@gmail.com>
2021-04-08 22:42:05 +0300

    feat: do 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,39 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let string = str;
+  const len = string.length - 1;
+  let counter = 1;
+  let letter = '';
+  string = string.split('').reduce((acc, itm, i) => {
+    const accum = acc;
+    const item = itm;
+    const iter = i;
+    if (iter === len) {
+      if (letter === item) {
+        counter++;
+        return accum + counter + letter;
+      }
+      if (counter === 1) { return accum + letter + item; }
+      return accum + counter + letter + item;
+    }
+    if (letter === item) {
+      counter++;
+      return accum;
+    }
+    if (counter === 1) {
+      const temp = letter;
+      letter = item;
+      return accum + temp;
+    }
+    const temp = letter;
+    const counterTemp = counter;
+    letter = item;
+    counter = 1;
+    return accum + counterTemp + temp;
+  }, '');
+  return string;
 }
 
 module.exports = encodeLine;
